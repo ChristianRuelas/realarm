202b7739a08d7e85bed7d13bc081f918dba13c52
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-10 13:17:03 -0700

    make it better

--- php/arduino.php
+++ php/arduino.php
@@ -23,10 +23,9 @@ die("Mongo DB no inicio");
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Document</title>
 </head>
 <body>
