02b443cf6a031e4891dfb211587ebf217e2774b5
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-10 12:39:13 -0700

    make it better

--- php/arduino.php
+++ php/arduino.php
@@ -1,7 +1,31 @@
 <?php
-
+require_once('../vendor/autoload.php');
 $id=$_POST['id'];
+$client = new MongoDB\Client('mongodb+srv://christian_realarm:rugc930730@christian0.nvkym.mongodb.net/Realarm?retryWrites=true&w=majority');
+if ($client) {
+    $db = $client->Realarm->usuarios;
+
+    $insertOneResult = $db->insertOne([
+    'nombre' => $id,
+    'apellido' => "ardu",
+    'clave' => "ardu",
+    'serie' => "ardu",
+    'pass' => "ardu",
+    
+    ]);
+
+    //$qry = array("serie" => $serie,"password" => $pass);
+  
+}else {
+die("Mongo DB no inicio");
+}
+
+
+
+
+
+
 //echo "<script> alert('se agrego el usuario')</script>";
-echo($id."  ese");
+//echo($id."  ese");
 
 ?>
