4eb2dcf7295b57c0ffbdb7f8e74c1411a598862f
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-20 12:24:47 -0700

    make it better

--- php/realarmver.php
+++ php/realarmver.php
@@ -18,7 +18,7 @@ if ($client) {
     ]);
     
     foreach($userDatabaseFind as $userFind) {
-        $dato = $userFind['dispositivobtnpress'];
+        $dato = $userFind['dispositivo[btnpress]'];
         var_dump($dato);
         
     }
