c76c13b3020de2a1b04d4ade8d1abe1ae2e0b7e2
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-20 11:45:05 -0700

    make it better

--- php/realarmver.php
+++ php/realarmver.php
@@ -16,6 +16,7 @@ if ($client) {
     $userDatabaseFind=$db->find([
         'dispositivo.serie'=>$serie,
     ]);
+    echo($userDatabaseFind['dispositivo.clave']);
     foreach($userDatabaseFind as $userFind) {
         $dato = $userFind['dispositivo.clave'];
         
