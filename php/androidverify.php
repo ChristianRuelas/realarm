8dc1eb2823e44c81f34cbce27b382fa7fad26483
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-24 13:33:34 -0700

    make it better

---
+++ php/androidverify.php
@@ -0,0 +1,36 @@
+<?php
+$serie= $_REQUEST['Rserie'];
+
+
+$client = new MongoDB\Client('mongodb+srv://christian_realarm:rugc930730@christian0.nvkym.mongodb.net/Realarm?retryWrites=true&w=majority');
+
+if ($client) {
+    $db = $client->Realarm->usuarios;
+    $userDatabaseFind=$db->find([
+        'serie'=>$serie,
+    ]);
+    foreach($userDatabaseFind as $userFind) {
+        $state = $userFind['estado'];
+        $act=$userFind['activar'];
+        
+        
+    }
+    if($state=="up"&&$act=="1"){
+        echo '1'; 
+        
+    }else if($state=="down"){
+        echo'2';
+        
+    }
+    else if($state=="up"&&$act=="0"){
+        echo'2';
+    }
+
+
+
+  
+}else {
+die("Mongo DB no inicio");
+}
+
+?>
