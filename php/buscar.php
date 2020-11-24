cddd22b3e37c6827d382ce9b0c661f9fc7847c0b
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-24 11:33:43 -0700

    make it better

--- php/buscar.php
+++ php/buscar.php
@@ -1,6 +1,6 @@
 <?php
 $serie=$_REQUEST['Rserie'];
-$datob=$_REQUEST['Rdato'];
+$dato=$_REQUEST['Rdato'];
 require_once('../vendor/autoload.php');
 $client = new MongoDB\Client('mongodb+srv://christian_realarm:rugc930730@christian0.nvkym.mongodb.net/Realarm?retryWrites=true&w=majority');
 if ($client) {
