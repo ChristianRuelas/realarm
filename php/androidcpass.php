2dd9ef6b2ae84030a7fb1bb93cfea998ea779d41
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-24 12:58:25 -0700

    make it better

--- php/androidcpass.php
+++ php/androidcpass.php
@@ -1,6 +1,6 @@
 <?php
 
-$serie=$_REQUEST["Ruser"];
+$serie=$_REQUEST["Rserie"];
 $pass=$_REQUEST["Rpass"];//contrasenia actual
 $pass2=$_REQUEST["Rpass2"];//nueva contrasenia
 $encrypted = password_hash($pass2,PASSWORD_DEFAULT);
@@ -8,16 +8,13 @@ require_once('../vendor/autoload.php');
 $client = new MongoDB\Client('mongodb+srv://christian_realarm:rugc930730@christian0.nvkym.mongodb.net/Realarm?retryWrites=true&w=majority');
 
 if ($client) {
-    echo $serie;
     $db = $client->Realarm->usuarios;
     $userDatabaseFind=$db->findOne([
         'serie'=>$serie,
     ]);
     $storedUsername = $userDatabaseFind['pass'];
-    //echo $storedUsername;
     
     if(password_verify($pass, $storedUsername)){
-        echo 'si es igual';
         $userDatabaseFind=$db->updateOne(
             ['serie' => $serie],
             ['$set' => ['pass' => $encrypted]]
