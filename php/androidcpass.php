e4c4fab950b0665d9ed2d0906a7ed98fca74f491
Christian Ruelas <shryshrox.punx@hotmail.com>
2020-11-24 12:43:32 -0700

    make it better

--- php/androidcpass.php
+++ php/androidcpass.php
@@ -8,12 +8,12 @@ require_once('../vendor/autoload.php');
 $client = new MongoDB\Client('mongodb+srv://christian_realarm:rugc930730@christian0.nvkym.mongodb.net/Realarm?retryWrites=true&w=majority');
 
 if ($client) {
-    echo 'si hay conexion';
     $db = $client->Realarm->usuarios;
     $userDatabaseFind=$db->findOne([
         'serie'=>$serie,
     ]);
     $storedUsername = $userDatabaseFind['pass'];
+    echo $storedUsername;
     if(password_verify($pass, $storedUsername)){
         echo 'si es igual';
         $userDatabaseFind=$db->updateOne(
